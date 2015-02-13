c5c07d093a998a950d0fe6cab92cf010875aca13
andifant <a.kleiber@reBuy.de>
2015-02-14 00:04:47 +0100

    basic directory structure

---
+++ app/app.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var andiwand = angular.module('andiwand', []);
---
+++ app/modules/about/aboutController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('andiwand').controller('aboutController', function() {
+  this.title = 'hello there';
+});
